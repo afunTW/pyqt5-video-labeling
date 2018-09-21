37cce5ca000a1991c53b0be4d6c16569dca1a17e
afunTW <endlessproof@gmail.com>
2018-09-21 14:54:23 +0800

    refactor: from treeview+model to tablewidget

--- src/view.py
+++ src/view.py
@@ -1,10 +1,11 @@
 import logging
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor, QFont, QPainter, QPen, QStandardItemModel
+from PyQt5.QtGui import QColor, QFont, QPainter, QPen
 from PyQt5.QtWidgets import (QAbstractItemView, QDesktopWidget, QGridLayout,
-                             QGroupBox, QHBoxLayout, QLabel, QPushButton,
-                             QSlider, QStyle, QTreeView, QVBoxLayout, QWidget)
+                             QGroupBox, QHBoxLayout, QHeaderView, QLabel,
+                             QPushButton, QSlider, QStyle, QTableWidget,
+                             QTableWidgetItem, QVBoxLayout, QWidget)
 
 
 class VideoFrameViewer(QLabel):
@@ -129,13 +130,16 @@ class VideoAppViewer(QWidget):
         vbox_option.addWidget(self.group_video_info)
 
         # vbox_option/table_preview_record: preview the summary of records
-        self.tree_preview_records = QTreeView()
-        self.tree_preview_records.setRootIsDecorated(False)
-        self.tree_preview_records.setAlternatingRowColors(True)
-        self.model_preview_records = self._get_preview_model(self)
-        self.tree_preview_records.setModel(self.model_preview_records)
-        self.tree_preview_records.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        vbox_option.addWidget(self.tree_preview_records)
+        self.table_preview_records = self._get_preview_table(self)
+        vbox_option.addWidget(self.table_preview_records)
+
+        # self.tree_preview_records = QTreeView()
+        # self.tree_preview_records.setRootIsDecorated(False)
+        # self.tree_preview_records.setAlternatingRowColors(True)
+        # self.model_preview_records = self._get_preview_model(self)
+        # self.tree_preview_records.setModel(self.model_preview_records)
+        # self.tree_preview_records.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # vbox_option.addWidget(self.tree_preview_records)
 
         # vbox_option/hbox_jump_records: jump to next or previous record
         hbox_jump_records = QHBoxLayout()
@@ -154,26 +158,22 @@ class VideoAppViewer(QWidget):
         label.setFont(self.font_header)
         label.setAlignment(Qt.AlignLeft)
         return label
-
-    def _get_preview_model(self, parent):
-        model = QStandardItemModel(0, 4, parent)
-        model.setHeaderData(0, Qt.Horizontal, 'timestamp')
-        model.setHeaderData(1, Qt.Horizontal, '#frame')
-        model.setHeaderData(2, Qt.Horizontal, 'pt1')
-        model.setHeaderData(3, Qt.Horizontal, 'pt2')
-        return model
     
-    def add_record_to_treeview(self, timestamp: str, frame_idx: int, pt1: tuple, pt2: tuple):
-        self.model_preview_records.insertRow(0)
-        col0_index = self.model_preview_records.index(0, 0)
-        col1_index = self.model_preview_records.index(0, 1)
-        col2_index = self.model_preview_records.index(0, 2)
-        col3_index = self.model_preview_records.index(0, 3)
-        self.model_preview_records.setData(col0_index, timestamp)
-        self.model_preview_records.setData(col1_index, frame_idx)
-        self.model_preview_records.setData(col2_index, str(pt1))
-        self.model_preview_records.setData(col3_index, str(pt2))
-        self.model_preview_records.sort(0)
+    def _get_preview_table(self, parent):
+        table = QTableWidget(parent=parent)
+        table.setColumnCount(4)
+        table.setHorizontalHeaderLabels(['timestamp', 'frame', 'pt1', 'pt2'])
+        table.setSortingEnabled(False)
+        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        return table
     
-    def remove_record_in_treeview(self, row_idx: int):
-        self.model_preview_records.removeRow(row_idx)
+    def add_record_to_preview(self, timestamp: str, frame_idx: int, pt1: tuple, pt2: tuple):
+        self.table_preview_records.insertRow(0)
+        self.table_preview_records.setItem(0, 0, QTableWidgetItem(timestamp))
+        self.table_preview_records.setItem(0, 1, QTableWidgetItem(str(frame_idx)))
+        self.table_preview_records.setItem(0, 2, QTableWidgetItem(str(pt1)))
+        self.table_preview_records.setItem(0, 3, QTableWidgetItem(str(pt2)))
+        self.table_preview_records.sortByColumn(0, Qt.AscendingOrder)
+    
+    def remove_record_from_preview(self, row_idx: int):
+        self.table_preview_records.removeRow(row_idx)
